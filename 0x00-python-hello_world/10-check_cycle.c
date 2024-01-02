9fc7f14d93c666067d0b29f1d0975ef589658eab
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-02 17:47:13 +0100

    Create 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+* check_cycle - checks if singly linked list has a cycle.
+* @list: head of list
+* Description - check for loops in list
+* Return: 1 (no-cycle) else 0 (cycle)
+*/
+
+int check_cycle(listint_t *list)
+{
+struct listint_t *t = list;
+struct listint_t *h = list->next;
+
+if (!list)
+return (0);
+
+while (h && t && h->next)
+{
+if (t == h)
+{
+return (1);
+}
+h = h->next;
+t = t->next->next;
+}
+return (0);
+}
