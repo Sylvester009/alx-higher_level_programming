1d2a7a357929de85678e392e6c21ac2acd157b82
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-04 16:03:47 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
     import sys
 
     count = len(sys.argv) - 1       # calculate commandline arguments
-    if count == 1:
-        print("1 arguments.")
-    elif count == 0:
-        print("0 argument:")
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
         print("{} arguments:".format(count))
     for i in range(count):      # iterate over the commandline arguments
