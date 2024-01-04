56677b3c059082db34216ce674472ae0b1027b20
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-04 15:43:40 +0100

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    #does maths and prints result
+    """does maths and prints result"""
     from calculator_1 import add, sub, mul, div
 
     a = 10
