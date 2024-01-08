76465997c6b3fbe34de81d61ca6481e1dfeadfb1
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-09 00:05:50 +0100

    tuple length

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
     tuple_len = ()
-   if not sentence:
-        tuple_len = 0, "None"
+    if not sentence:
+        tuple_len = 0, None
     else:
         tuple_len = len(sentence), sentence[0]
     return tuple_len
