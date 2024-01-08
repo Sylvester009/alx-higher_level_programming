e93320dfa103adfd5565da2907b854515b992e45
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-09 00:11:36 +0100

    max_value

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+      
+    else:
+        max_value = my_list[0]
+        for num in range(len(my_list)):
+            if my_list[num] > max_value:
+                max_value = my_list[num]
+        return max_value
