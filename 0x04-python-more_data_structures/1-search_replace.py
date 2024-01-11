c13102df4e20128419d2ab7fe972fa85e6a9c4a5
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-11 04:25:17 +0100

    Create 1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+def search_replace(my_list, search, replace):
+    '''a function that replaces all occurrences of an element
+    by another in a new list'''
+    newList = []
+
+    for element in my_list:
+        if element == search:
+            newList.append(replace)
+        else:
+            newList.append(element)
+
+    return newList
