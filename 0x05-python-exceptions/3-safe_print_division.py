3f08c020e9c0e0aef12913e857fd928a25d6ff5f
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-23 00:12:45 +0100

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,9 @@
 def safe_print_division(a, b):
     """
     Divides 2 integers and prints the result.
-
     Args:
         a: Integer.
         b: Integer.
-
     Returns:
         The value of the division, otherwise None.
     """
