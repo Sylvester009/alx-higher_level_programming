1b7e85fe066b47403c49ffe70c341e79cc90a47b
Sylvester009 <samsylvester09@gmail.com>
2024-01-23 17:43:57 -0800

    creation of 3 square classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""class that defines a square"""
+
+class Square:
+
+    def __init__(self, size):
+        """Args:
+        size: size of square
+        """
+        self.__size = size
