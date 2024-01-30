6314dde77ebf9f478368ea9c51d9f6d293e2f235
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-30 17:04:43 +0100

    Create 5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# 5-text_indentation.py
+"""Defines a text-indentation function."""
+
+
+def text_indentation(text):
+    """prints a text with 2 new lines after characters: ., ? and :
+    Args:
+        text (str): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    b = 0
+    while c < len(text) and text[c] == ' ':
+        b += 1
+
+    while b < len(text):
+        print(text[b], end="")
+        if text[b] == "\n" or text[b] in ".?:":
+            if text[b] in ".?:":
+                print("\n")
+            b += 1
+            while c < len(text) and text[c] == ' ':
+                b += 1
+            continue
+        b += 1
