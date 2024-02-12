4c1ce659b7b5bfad38e1e39f789c89f14b029d81
Samuel Sylvester <samsylvester09@gmail.com>
2024-02-12 14:23:11 +0100

    added update to rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -101,3 +101,23 @@ class Rectangle(Base):
         """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height)
+        
+    def update(self, *args):
+        """assigns an argument to each attribute:
+        Args:
+            1st arg: id attribute
+            2nd arg: width attribute
+            3rd arg: height attribute
+            4th arg: x attribute
+            5th arg: y attribute
+        """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
