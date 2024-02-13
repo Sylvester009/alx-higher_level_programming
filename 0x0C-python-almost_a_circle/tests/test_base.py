2d15488bfff43063a7c7cb51902b4a7e3afd9b37
Samuel Sylvester <samsylvester09@gmail.com>
2024-02-13 01:17:57 +0100

    Update test_base.py

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -3,7 +3,6 @@
 
 """
 Unittest for Base Class
-"""
 
 import unittest
 from models.base import Base
@@ -24,3 +23,56 @@ class TestBase(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+    """
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+import os
+
+
+class TestBase(unittest.TestCase):
+
+    def test_create_rectangle(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        r1_dict = r1.to_dictionary()
+        r2_dict = r2.to_dictionary()
+        
+        # Save rectangles to file
+        Rectangle.save_to_file([r1, r2])
+        
+        # Load rectangles from file
+        loaded_rectangles = Rectangle.load_from_file()
+        
+        # Check if loaded rectangles are equal to original ones
+        self.assertEqual(len(loaded_rectangles), 2)
+        self.assertEqual(loaded_rectangles[0].to_dictionary(), r1_dict)
+        self.assertEqual(loaded_rectangles[1].to_dictionary(), r2_dict)
+        
+        # Clean up created file
+        os.remove("Rectangle.json")
+
+    def test_create_square(self):
+        s1 = Square(5)
+        s2 = Square(7, 9, 1)
+        s1_dict = s1.to_dictionary()
+        s2_dict = s2.to_dictionary()
+        
+        # Save squares to file
+        Square.save_to_file([s1, s2])
+        
+        # Load squares from file
+        loaded_squares = Square.load_from_file()
+        
+        # Check if loaded squares are equal to original ones
+        self.assertEqual(len(loaded_squares), 2)
+        self.assertEqual(loaded_squares[0].to_dictionary(), s1_dict)
+        self.assertEqual(loaded_squares[1].to_dictionary(), s2_dict)
+        
+        # Clean up created file
+        os.remove("Square.json")
+
+if __name__ == '__main__':
+    unittest.main()
+
