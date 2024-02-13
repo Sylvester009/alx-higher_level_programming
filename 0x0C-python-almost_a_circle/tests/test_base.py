ccde57d3714fca3f04059f5a3710041148461315
Samuel Sylvester <samsylvester09@gmail.com>
2024-02-13 01:50:11 +0100

    Update test_base.py

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -3,6 +3,8 @@
 
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 class TestBase(unittest.TestCase):
     def test_init_with_id(self):
