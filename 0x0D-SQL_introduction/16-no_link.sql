8b66782d0d41b372ca6227a9b2e6ba7066e4461a
Samuel Sylvester <samsylvester09@gmail.com>
2024-03-12 23:53:28 +0100

    Create 16-no_link.sql

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,4 @@
+-- script that lists all records of the table second_table
+-- Don’t list rows without a name value
+
+alx-higher_level_programming/0x0D-SQL_introduction/16-no_link.sql at main · hunterxcobby/alx-higher_level_programming
