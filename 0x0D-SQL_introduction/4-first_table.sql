3bc846bf3d3731674bf5df70df5983fb4df1ebe7
Sylvester009 <samsylvester09@gmail.com>
2024-03-12 14:40:53 -0700

    Create a Table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- First table
+-- a script that creates a table called first_table
+-- Table is created  in the current database in my MySQL server
+
+CREATE TABLE first_table (id INT, name VARCHAR(256));
