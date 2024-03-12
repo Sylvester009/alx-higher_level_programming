25854acc8643ff74d70a55c19296820f40f29307
Samuel Sylvester <samsylvester09@gmail.com>
2024-03-12 23:57:37 +0100

    Delete 0x0D-SQL_introduction/16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++
@@ -1,4 +0,0 @@
--- script that lists all records of the table second_table
--- Don’t list rows without a name value
-
-SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
