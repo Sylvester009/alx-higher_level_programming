5eddefbb9e59801797886f26f9a8deae9da1474c
Samuel Sylvester <samsylvester09@gmail.com>
2024-03-12 23:54:36 +0100

    Update 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- script that lists all records of the table second_table
 -- Don’t list rows without a name value
 
-alx-higher_level_programming/0x0D-SQL_introduction/16-no_link.sql at main · hunterxcobby/alx-higher_level_programming
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
