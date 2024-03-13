983a106ea50e6fe7b7d2948e23bf178e5934e29e
Sylvester009 <samsylvester09@gmail.com>
2024-03-13 12:54:10 -0700

    first commit

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- My privileges!
+-- script that lists all privileges of the MySQL users 
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,4 @@
+-- ID can't be null
+-- a script that creates a table called id_not_null
+
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
