6a731d00c3aec71aa6d74d80e628058d43f602c6
Samuel Sylvester <samsylvester09@gmail.com>
2024-04-08 22:25:28 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,6 @@
 const process = require('node:process');
 
 const argCount = process.argv.length - 2;
-const message = argCount === 1 ? 'Argument found' : argtCount < 1 ? 'No argument' : 'Arguments found';
+const message = argCount === 1 ? 'Argument found' : argCount < 1 ? 'No argument' : 'Arguments found';
 
 console.log(message);
