6c995cde97749a4cd7f1e8141f188ac8f8a145f0
Samuel Sylvester <samsylvester09@gmail.com>
2024-04-08 23:15:09 +0100

    Create 5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const process = require('node: process');
+const arg = process.argv[2];
+const number = parseInt(arg);
+const message = !isNaN(number) ? `My number: ${number}` : 'Not a number';
+
+console.log(message);
