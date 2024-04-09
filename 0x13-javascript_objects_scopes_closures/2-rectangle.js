dcbc4bf67c98bf5ea0d8de36cc78a108a80d8dc1
Samuel Sylvester <samsylvester09@gmail.com>
2024-04-09 12:54:05 +0100

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,10 @@
 
 class Rectangle {
   constructor(w, h) {
-    if (w > 0 || h > 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return {};
+    if (w > 0 && h > 0) {
+      [this.width, this.height] = [w, h];
     }
-    this.width = w;
-    this.height = h;
   }
 }
+
 module.exports = Rectangle;
