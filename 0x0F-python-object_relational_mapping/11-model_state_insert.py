02f768c29c812a48d962519274cb9bf82c0b1ad0
Samuel Sylvester <samsylvester09@gmail.com>
2024-04-15 23:44:59 +0100

    Update 11-model_state_insert.py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-  
+
     session = Session(engine)
     new = State(name='Louisiana')
     session.add(new)
