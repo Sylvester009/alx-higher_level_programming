63f4a9d48a16b86278c18068fb64dc3e911ce548
Samuel Sylvester <samsylvester09@gmail.com>
2024-04-15 22:24:47 +0100

    my safe filter states

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script takes arguments and displays all values in
+table  where name matches the argument, safe from MySQL
+"""
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         username=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name=%s\
+                ORDER BY states.id ASC", (sys.argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
