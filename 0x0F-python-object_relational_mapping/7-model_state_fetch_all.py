b871f82853450559e80675cfa3c109968291c4b5
Samuel Sylvester <samsylvester09@gmail.com>
2024-04-15 23:10:38 +0100

    Update 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,10 +10,13 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     if len(sys.argv) < 4:
-        print("Usage: {} <username> <password> <dbname>".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <dbname>"
+              .format(sys.argv[0]))
         sys.exit(1)
 
-    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}', pool_pre_ping=True)
+    engine = create_engine(
+        f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}',
+        pool_pre_ping=True)
 
     with Session(engine) as session:
         for state in session.query(State).order_by(State.id).all():
