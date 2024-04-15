a829ccba84a9a389e9857aa6a0a3d3a992ce68c6
Samuel Sylvester <samsylvester09@gmail.com>
2024-04-15 23:19:20 +0100

    Update 9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
                                    sys.argv[3]), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
-    
+
     # Create a session to interact with the database
     session = Session(engine)
     for state in session.query(State)\
