e9db853cd2a4b764af2f78bfcc0b7d8112ae2d08
Samuel Sylvester <samsylvester09@gmail.com>
2024-04-26 22:03:00 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,14 +2,13 @@
 """
 Simple script to fetch and display the status from an intranet URL.
 """
-
 import urllib.request
 
-if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type:', type(html))
-        print('\t- content:', html)
-        print('\t- utf8 content:', html.decode("utf-8", "replace"))
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+    print("Body response:")
+    print("\t- type:", type(html))
+    print("\t- content:", html)
+    print("\t- utf8 content:", html.decode('utf-8'))
