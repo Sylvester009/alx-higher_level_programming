ee2dd1aea3d0d2f42d5b6c83ecc0b220d2d701b9
Samuel Sylvester <samsylvester09@gmail.com>
2024-04-26 23:19:00 +0100

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,3 +4,16 @@ takes your GitHub credentials
 (username and password)
 and uses the GitHub API to display your id
 """
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    res = requests.get("https://api.github.com/user", auth=(username, password))
+
+    if res.status_code == 200:
+        print(res.json()["id"])
+    else:
+        print(None)
