ace5532461fa650b3dc2d91bf99a06f46181da05
Samuel Sylvester <samsylvester09@gmail.com>
2024-04-26 23:10:09 +0100

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,3 +4,17 @@ takes in a letter and sends a POST request to
 http://0.0.0.0:5000/search_user
 with the letter as a parameter.
 """
+if __name__ == "__main__":
+    import requests
+    import sys
+  
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    res = requests.post('http://0.0.0.0:5000/search_user', data={'q': letter})
+    try:
+        res_data = res.json()
+        if res_data == {}:
+            print('No result')
+        else:
+            print("[{}] {}".format(res_data.get('id'), res_data.get('name')))
+    except ValueError:
+        print('Not a valid JSON')
