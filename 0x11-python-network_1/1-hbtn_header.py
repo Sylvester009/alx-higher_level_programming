fcaf2eec3ac0ec1065335e91767b46d0c86747cb
Samuel Sylvester <samsylvester09@gmail.com>
2024-04-26 22:09:08 +0100

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,3 +2,12 @@
 """
 takes in a URL, sends a request to the URL and displays X-Request-Id
 """
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        header = response.info()
+        print(header['X-Request-Id'])
