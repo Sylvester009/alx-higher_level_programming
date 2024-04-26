b984d206a4c7cdc17037fc12eb0980770a1bd110
Samuel Sylvester <samsylvester09@gmail.com>
2024-04-26 03:09:34 +0100

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a request to the server
-curl -s -X POST "http://0.0.0.0:5000/catch_me" -d "user_id=yougotme" >/dev/null 2>&1
+curl -s -X POST "http://0.0.0.0:5000/catch_me" -d "user_id=yougotme"
