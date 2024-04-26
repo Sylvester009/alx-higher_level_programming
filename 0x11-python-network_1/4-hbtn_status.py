c135913d5c9190c29d4a6fcb155c7dce65c4b90a
Samuel Sylvester <samsylvester09@gmail.com>
2024-04-26 22:48:55 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,3 +2,12 @@
 """
 fetches https://alx-intranet.hbtn.io/status
 """
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    res = requests.get(url)
+
+    print("Body response:")
+    print("\t- type:", type(res.text))
+    print("\t- content:", res.text)
