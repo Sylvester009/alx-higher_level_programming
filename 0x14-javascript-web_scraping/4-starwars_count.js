01684186a30cc497b3906f6eeecd46bc235432a5
Samuel Sylvester <samsylvester09@gmail.com>
2024-05-22 02:05:24 +0100

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 // Star wars Wedge Antilles
 const req = require('request');
-const url = process.argv[2];
 let number = 0;
 
 
-req.get(url, (err, res, body) => {
+req.get(process.argv[2], (err, res, body) => {
   if (err) {
     console.log(err);
   } else {
-    const content = JSON.parse(body);
-    content.results.forEach((film) => {
+    const cont = JSON.parse(body);
+    cont.results.forEach((film) => {
       film.characters.forEach((character) => {
         if (character.includes(18)) {
           number += 1;
