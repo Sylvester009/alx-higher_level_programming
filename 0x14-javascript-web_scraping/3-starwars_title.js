379aa40be4457c35e6e4db1101b83ac4082d69a7
Samuel Sylvester <samsylvester09@gmail.com>
2024-05-22 01:53:02 +0100

    Create 3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Star wars movie title
+
+const req = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request.get(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const content = JSON.parse(body);
+    console.log(content.title);
+  }
+});
