2a660ae3299f21325b7c7274770be855d20b8078
Samuel Sylvester <samsylvester09@gmail.com>
2024-05-22 02:10:31 +0100

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,15 +3,14 @@
 const req = require('request');
 let number = 0;
 
-
-req.get(process.argv[2], (err, res, body) => {
+req.get(process.argv[2], (err, resp, body) => {
   if (err) {
     console.log(err);
   } else {
     const cont = JSON.parse(body);
-    cont.results.forEach((film) => {
-      film.characters.forEach((character) => {
-        if (character.includes(18)) {
+    cont.results.forEach((movie) => {
+      movie.characters.forEach((char) => {
+        if (char.includes(18)) {
           number += 1;
         }
       });
