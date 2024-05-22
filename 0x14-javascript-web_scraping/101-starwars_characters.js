0996e38699349a062e7fc75a00055dbfc92ce009
Samuel Sylvester <samsylvester09@gmail.com>
2024-05-22 02:40:13 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,20 +3,24 @@
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request.get(url, (error, response, body) {
-  if (!err) {
-    const chars = JSON.parse(body).characters;
+request.get(url, (error, response, body) => {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
     printChars(characters, 0);
+  } else {
+    console.log(error);
   }
 });
 
-function printChars (characters, index) {
-  request.get(characters[index], (err, resp, body) {
+function printChars(characters, index) {
+  if (index >= characters.length) return;
+
+  request.get(characters[index], (err, resp, body) => {
     if (!err) {
       console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printChars(characters, index + 1);
-      }
+      printChars(characters, index + 1);
+    } else {
+      console.log(err);
     }
   });
 }
