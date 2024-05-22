a774a49f1ace8a13b89735e804dc85b2dc43b23b
Samuel Sylvester <samsylvester09@gmail.com>
2024-05-22 02:19:26 +0100

    starwars characters

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+// Right order
+const req = require('request');
+const identity = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${identity}`;
+
+req.get(url, (err, resp, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const content = JSON.parse(body);
+    const chars = content.characters;
+    for (const char of chars) {
+      req.get(char, (err, resp, body) => {
+        if (err) {
+          console.log(err);
+        } else {
+          const contNames = JSON.parse(body);
+          console.log(contNames.name);
+        }
+      });
+    }
+  }
+});
